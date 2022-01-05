66cac5ddb7b9307e9e5b0a00555c471676e8bb44
Ibwedagama <wardana.wedagama@gmail.com>
2022-01-05 22:30:08 +0700

    chore (home): fetch and show data from api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,33 @@
-<template>
+ <template>
+  <!-- eslint-disable -->
   <div class="home">
-    <h1>Vue Mocking with MSW</h1>
+    <h1>Vue API Mock with MSW</h1>
+    <br />
+    <h2>Post Data</h2>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async mounted() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts?_limit=10"
+    );
+
+    this.posts = response.data;
+  },
 };
 </script>
